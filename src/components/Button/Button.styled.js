7179ea74a97c899ffb898970174d96e928b995fe
Brunnalage <brunnalage@gmail.com>
2022-07-05 23:27:42 -0300

    Style: adding all button component styles

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`    
+    padding: 0.688rem 0.75rem;
+    height: 2.5rem;
+    background: #074EE8;
+    border-radius:0.25rem;
+    border: none;
+    color: #FFFFFF;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;  
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center; 
+    gap: 6px;
+    margin-top: 15px;
+`;
