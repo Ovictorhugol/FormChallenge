a7485527cc4041aa6a5c4347114aafa571c0c7f3
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-07 17:50:40 -0300

    Bug:Bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import Text from "./components/Text/Text";
+
 function App() {
-  return (
-    <div>
-      <Text title="Your data has been send successfully" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-      <Text title="Full name: " variable="eqwiodjahsiudhas" />
-    </div>
-  );
+  return <div>HELLO</div>;
 }
 
 export default App;
