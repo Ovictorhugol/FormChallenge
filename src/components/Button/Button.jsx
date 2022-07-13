2cfd6c6eabf2ef23e4b4245390f4bec97cb41352
Brunna Lage <91227939+Brunnalage@users.noreply.github.com>
2022-07-13 13:01:13 -0300

    Refactor: redoing "More" button structure, adding error message and correcting "onclick" which was in the wrong place

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import  {StyledButton} from './Button.styled';
-
+import { LabelError } from '../Input/Input.styled';
 import arrowIcon from '../../assets/icons/arrowIcon.svg'
 import addIcon from '../../assets/icons/addIcon.svg';
 import checkIcon from '../../assets/icons/checkIcon.svg';
 import downIcon from '../../assets/icons/downIcon.svg';
 
-const Button = ({type, id, title, onClick } ) => {
+const Button = ({type, id, title, onClick, hasError } ) => {
  switch (id) {
     case "Next":
     return<StyledButton className={`${id}`} type={type} id={id} onClick={onClick}>{title}<img src={arrowIcon}></img> </StyledButton>
     case "More":
-    return<StyledButton type={type} id={id}><img src={addIcon} onClick={onClick}></img>{title}<img src={arrowIcon}></img> </StyledButton>
+    return<>
+    <StyledButton className={`${id}`} type={type} id={id} onClick={onClick}><img src={addIcon}></img>{title}<img src={arrowIcon}></img> </StyledButton>
+    {hasError && <LabelError>{`Please enter your ${id}`}</LabelError>}
+    </>
     case "Ending":
-    return<StyledButton type={type} id={id}><img src={checkIcon} onClick={onClick}></img>{title}</StyledButton>
+    return<StyledButton className={`${id}`} type={type} id={id} onClick={onClick}><img src={checkIcon} ></img>{title}</StyledButton>
     case "Certificates":
     return<StyledButton className={`${id}`} type={type} id={id} onClick={onClick}>{title}<img src={downIcon}></img> </StyledButton>
  }
 }
-
 export default Button;
+
