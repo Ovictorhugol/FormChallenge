abb4c6018fc47442c4be25263115aa755361a11a
Brunnalage <brunnalage@gmail.com>
2022-07-14 00:39:17 -0300

    Refactor: adding new logic to add item to list

--- src/components/Containers/Certificates/Certificates.jsx
+++ src/components/Containers/Certificates/Certificates.jsx
@@ -1,47 +1,50 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Input from '../../Input/Input';
 import Button from '../../Button/Button';
 import SplitButton from '../../SplitButton/SplitButton';
-import { CertificatesForm, CertificatesInput, SubmitButton, DataInputs } from './Certificates.styled';
+import styled from "styled-components";
+import { CertificatesForm, CertificatesInput, SubmitButton, DataInputs, LiStyled } from './Certificates.styled';
 
 
-function addItem() {
-    var a = document.getElementById("list");
-    var certificates = document.getElementById("certificates");
-    var li = document.createElement("li");
-    var btn = document.createElement('button');
-    btn.innerText="X";
-    btn.type="button";
-    btn.onclick= removeItem;
-    li.setAttribute('id', certificates.value);
-    li.appendChild(document.createTextNode(certificates.value));   
-    li.appendChild((btn))
-    a.appendChild(li);        
-}
 
-function removeItem() {   
-    var a = document.getElementById("list");   
-    var item = document.querySelectorAll("#list li");   
-    a.removeChild(item[0])
-}
-const Certificates = () => {
-    return(        
-            <CertificatesForm >
-                <CertificatesInput>
-                <Input id="certificates" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" type="text" label="Certificates"/>
-                </CertificatesInput>
-                <SplitButton id="list" />
-                <Button type="button" id="More" title="More" onClick={addItem}/>
-                <DataInputs>
-                <Input id="TeamName" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" type="text" label="Team Name*"/>
-                <Input id="Institution" placeholder="Universidade Federal da Paraíba" type="text" label="Institution*"/>
-                <Input id="Graduation" placeholder="Ciências da Computação" type="text" label="Graduation*"/>                
-                </DataInputs>
-                <SubmitButton>
-                <Button type="button" id="Ending" title="Finish"/>                
-                </SubmitButton>
-            </CertificatesForm>        
+const Certificates = () => {   
+    const [certificates, setCertificates] = useState([]);
+    const [inputContent, setInputContent] = useState('');
+
+    function handleChange(event) {
+        setInputContent(event.target.value);
+    }
+
+    function handleClick() {
+        setCertificates([...certificates, inputContent])              
+    }
+
+    function removeItem() {   
+        const certificateList = document.getElementById("list");   
+        const item = document.querySelectorAll("#list li");   
+        const removeBtn = document.querySelectorAll("#list Button");   
+        certificateList.removeChild(item[0])
+        certificateList.removeChild(removeBtn[0])
+    }
+
+    return (
+        <CertificatesForm >
+            <CertificatesInput>
+                <Input id="certificates" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" type="text" label="Certificates" onChange={handleChange}/>
+            </CertificatesInput>
+            <SplitButton id="list" list={certificates.map(certificate => <><LiStyled key={certificate}>{certificate}</LiStyled><Button type="button" id="Remove" title="...&emsp;X" onClick={removeItem}/></>)}/>
+            <Button type="button" id="More" title="More" onClick={handleClick} />
+            <DataInputs>
+                <Input id="TeamName" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" type="text" label="Team Name*" />
+                <Input id="Institution" placeholder="Universidade Federal da Paraíba" type="text" label="Institution*" />
+                <Input id="Graduation" placeholder="Ciências da Computação" type="text" label="Graduation*" />
+            </DataInputs>
+            <SubmitButton>
+                <Button type="button" id="Ending" title="Finish" />
+            </SubmitButton>
+        </CertificatesForm>
     )
 }
 
 export default Certificates;
+
