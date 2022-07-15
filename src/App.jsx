30f3eb4047bb2de5b2e54309788135aec8a80836
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-15 15:26:55 -0300

    Bug:bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React, {useState} from "react";
-import Header from "./components/Containers/Header/Header"
-import Social from "./components/Containers/Form/Social/Social"
+import React, { useState } from "react";
+import Header from "./components/Containers/Header/Header";
+import Social from "./components/Containers/Form/Social/Social";
 import { Routes, Route } from "react-router-dom";
-
-
+import Tabs from "./components/Tabs/Tabs";
 function App() {
-  const [currentTab, setCurrentTab] = useState (1)
-  
+  const [currentTab, setCurrentTab] = useState(1);
+
   return (
     <>
       <Routes>
-          <Route path="/" element={<Social />} />
-          <Route path="header" element={<Header currentTab={currentTab}/>} />
-      </Routes>  
+        <Route path="/" element={<Tabs activeTab={1} />} />
+        <Route path="header" element={<Header currentTab={currentTab} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
