5ccd2007eb5920ba02a2e3cd3aa57fb1b22040dc
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-15 15:14:49 -0300

    Bug:bugfix

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -12,70 +12,68 @@ import {
   InputStyleCheckbox,
 } from "./Input.styled.js";
 
-const Input = ({ 
-    id, 
-    placeholder, 
-    type, 
-    label, 
-    value, 
-    onChange,
-    hasError
-    }) => {
-        const getInput = (type) => {
-            switch (type) {
-                case "text":
-                case "email":
-                case "number":
-                    return <Container>
-                            <LabelContainer>
-                                <Label htmlFor={id}>{label}</Label>
-                                {hasError && <LabelError>{`Please enter your ${id}`}</LabelError>}
-                            </LabelContainer>
-                            <InputStyle 
-                                id={id} 
-                                placeholder={placeholder} 
-                                type={type} 
-                                value={value} 
-                                onChange={onChange}      
-                            /> 
-                        </Container>; 
-                case "checkbox":
-                    return (
-                      <CheckboxContainer>
-                        <InputStyleCheckbox
-                          id={id}
-                          placeholder={placeholder}
-                          type={type}
-                          value={value}
-                          onChange={onChange}
-                        />
-                        <LabelCheckBox for={id} id="Accept">
-                          {label}
-                        </LabelCheckBox>
-                        {hasError && (
-                          <CheckboxError>{"Please confirm the terms"}</CheckboxError>
-                        )}
-                      </CheckboxContainer>
-                    );
-                case "url":
-                    return <Container>
-                            <LabelContainer>
-                                <Label>{label}</Label>
-                                {hasError && <LabelError>{`Please enter your ${id} Link`}</LabelError>}
-                            </LabelContainer>
-                            <InputStyle 
-                                id={id} 
-                                placeholder={placeholder} 
-                                type={type} 
-                                value={value} 
-                                onChange={onChange}      
-                            /> 
-                        </Container>;
-                default:
-                    return <Input>{label}</Input>;
-            }            
-        }
-        return <div>{getInput(type)}</div>;
-
+const Input = ({ id, placeholder, type, label, value, onChange, hasError }) => {
+  const getInput = (type) => {
+    switch (type) {
+      case "text":
+      case "email":
+      case "number":
+        return (
+          <Container>
+            <LabelContainer>
+              <Label htmlFor={id}>{label}</Label>
+              {hasError && <LabelError>{`Please enter your ${id}`}</LabelError>}
+            </LabelContainer>
+            <InputStyle
+              id={id}
+              placeholder={placeholder}
+              type={type}
+              value={value}
+              onChange={onChange}
+            />
+          </Container>
+        );
+      case "checkbox":
+        return (
+          <CheckboxContainer>
+            <InputStyleCheckbox
+              id={id}
+              placeholder={placeholder}
+              type={type}
+              value={value}
+              onChange={onChange}
+            />
+            <LabelCheckBox for={id} id="Accept">
+              {label}
+            </LabelCheckBox>
+            {hasError && (
+              <CheckboxError>{"Please confirm the terms"}</CheckboxError>
+            )}
+          </CheckboxContainer>
+        );
+      case "url":
+        return (
+          <Container>
+            <LabelContainer>
+              <Label>{label}</Label>
+              {hasError && (
+                <LabelError>{`Please enter your ${id} Link`}</LabelError>
+              )}
+            </LabelContainer>
+            <InputStyle
+              id={id}
+              placeholder={placeholder}
+              type={type}
+              value={value}
+              onChange={onChange}
+            />
+          </Container>
+        );
+      default:
+        return <Input>{label}</Input>;
+    }
+  };
+  return <div>{getInput(type)}</div>;
+};
 
 export default Input;
