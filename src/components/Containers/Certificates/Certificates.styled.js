d96fc6336714c13b1ab0965aed649a844b5700cf
Brunnalage <brunnalage@gmail.com>
2022-07-16 15:50:44 -0300

    Style: adding style in "plus" button error message and styles for active and inactive split button div

--- src/components/Containers/Certificates/Certificates.styled.js
+++ src/components/Containers/Certificates/Certificates.styled.js
@@ -5,7 +5,7 @@ export const CertificatesForm = styled.form`
     margin-right: ${toRem(35)};
     margin-left: ${toRem(67)};
     display: grid;
-    grid-template-columns: 50% 50%;
+    grid-template-columns: 22.5% 77.5%;
     grid-template-rows: auto ${toRem(77)} auto auto;
 `;
 
@@ -39,4 +39,37 @@ export const LiStyled = styled.li`
     padding-left: ${toRem(8)};          
 `;
 
+export const ErrorMessage = styled.p`
+    font-family: "Nunito", sans-serif;
+    font-size: ${toRem(14)};
+    text-align: right;
+    color: #FF0000;
+    font-weight: 400;  
+    margin-top: ${toRem(5)};
+    max-width: 30ch;
+    line-height: ${toRem(18)};     
+    &.limit {
+    opacity: 0;
+    &.active {
+    opacity: 1;
+    visibility: visible;    
+    }
+    }
+`;
 
+export const MoreBtn = styled.div`
+    display: grid;
+    justify-items: end;
+`;
+
+export const SplitButtonDiv = styled.div`
+    &.btnActive {
+    opacity: 0;
+    visibility: hidden;
+    &.active {
+    opacity: 1;
+    visibility: visible;        
+    }
+    }
+    z-index: 10;
+`;
