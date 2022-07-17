cd18b1ea2c6b1e3e3f6a304dc705b813da808ac4
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-17 18:44:04 -0300

    Bug:bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
-import Header from "./components/Containers/Header/Header";
-import Social from "./components/Containers/Form/Social/Social";
 import { BrowserRouter as Router } from "react-router-dom";
-import Tabs from "./components/Tabs/Tabs";
-import { Container } from "./App.styled";
 import Routes from "./utils/routes";
 function App() {
-  const [currentTab, setCurrentTab] = useState(1);
-
   return (
     <Router>
       <Routes />
