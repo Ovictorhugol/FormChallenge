baec6936744e2a5da036e2a345bf7894880dc2cd
Brunnalage <brunnalage@gmail.com>
2022-07-17 23:46:20 -0300

    Style: adding grid and adjusting normal and responsive container styles

--- src/App.styled.js
+++ src/App.styled.js
@@ -4,11 +4,11 @@ export const Container = styled.div`
   display: flex;
   background: white;
   border-radius: ${toRem(20)};
-  width: 34rem;
-  height: 110vh;
-  margin-top: 2vh;
-  flex-direction: column;
+  width: ${toRem(617)};
   margin: auto;
+  max-height: ${toRem(763)};
+  margin-top: ${toRem(69)};
+  flex-direction: column;  
   padding: auto;
 
   @media (max-width: 768px) {
--- src/components/Containers/Basic/Basic.styled.js
+++ src/components/Containers/Basic/Basic.styled.js
@@ -1,56 +1,95 @@
 import styled from "styled-components";
 import { toRem } from "../../../utils/convertToRem";
 
-export const ContainerBasic = styled.div`
-  width: 30rem;
-  display: flex;
-  flex-wrap: wrap;
+export const ContainerBasic = styled.div`  
+  margin-right: ${toRem(55)};
+  margin-left: ${toRem(47)};
+  display: grid;
+  grid-template-columns: 68.6% 31.4%;
+  grid-template-rows: auto auto auto auto auto auto;
 
   justify-content: space-between;
-  @media (max-width: 768px) {
-    width: 90%;
+  @media (max-width: 768px) {   
+    grid-template-columns: 50% 50%;
+    grid-template-rows: auto auto auto auto auto auto auto auto;
   }
 `;
 
 export const ContainerInputLarger = styled.div`
-  width: 30rem;
+  grid-column: 1 / 3;    
 `;
 
 export const ContainerInputLargerEmail = styled.div`
-  width: 30rem;
+  grid-column: 1 / 2;  
+  grid-row: 3 /4;
+  margin-right: 1.5rem;
   @media (max-width: 768px) {
-    width: 50%;
+    grid-column: 1 / 3; 
+    margin-right: 0;
   }
 `;
+
 export const ContainerInputLargerPhone = styled.div`
-  width: 30rem;
+  grid-column: 2 / 3;  
+  grid-row: 3 / 4;
   @media (max-width: 768px) {
-    width: 35%;
+    grid-column: 1 / 3;
+    grid-row: 4 / 5;
   }
 `;
-export const ContainerBirthdayLabel = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+
+export const ContainerBirthdayLabel = styled.div` 
+  grid-column: 1 / 3; 
+  grid-row: 4 / 5;
+  width: 100%;  
+  @media (max-width: 768px) {    
+    grid-row: 5 / 6;
+  }
 `;
+
 export const ContainerBirthday = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
+  grid-column: 1 / 3; 
+  grid-row: 4 / 5;
+  display: grid;
+  grid-template-columns: 21% 21% 21% auto;    
+  gap: 1.375rem;
+  @media (max-width: 768px) {
+    grid-column: 1 / 3; 
+    grid-row: 4 / 6;   
+    display: grid;
+    grid-template-columns: 47% 47%;
+    grid-template-rows: 50% 50%;
+    grid-gap: 0 1.375rem;
+    
+  }
 `;
 
 export const ContainerBirthdayInput = styled.div`
-  width: 5rem;
-  height: 4rem;
-  @media (max-width: 768px) {
-    width: 25vw;
+  grid-column: 4/ 5; 
+  margin-left: 0.375rem;
+
+  @media (max-width: 768px) {    
+    grid-column: 2 / 3;   
   }
 `;
-export const ContainerCheckbox = styled.div``;
+
+export const ContainerCheckbox = styled.div`
+  grid-column: 1 / 3; 
+  grid-row: 5 / 6;
+  margin-bottom: 2.688rem;
+  @media (max-width: 768px) {   
+    grid-row: 6/ 7;  
+  }
+`;
+
 export const ContainerButton = styled.div`
+  grid-column: 2 / 3; 
+  grid-row: 6 / 7;
   display: flex;
   width: 100%;
   justify-content: flex-end;
+  margin-bottom: 2.25rem;
+  @media (max-width: 768px) {
+    grid-row: 7/ 8;   
+  }
 `;
