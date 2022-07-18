58ce817bb6d58784ee1d505bb204793040365b08
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-18 16:55:44 -0300

    Feat:404 Error page styling

--- src/components/Pages/404/Error404.styled.js
+++ src/components/Pages/404/Error404.styled.js
@@ -4,9 +4,13 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: rgb(240, 240, 240);
 `;
 
 export const Error404 = styled.img`
   max-width: 100vw;
   max-height: 100vh;
+  display: flex;
 `;
